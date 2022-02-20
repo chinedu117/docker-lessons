7641f3fc8c2547b7bdd124f29d7f20edb368d930
chinedu117 <chinedudimonyeka@gmail.com>
2022-02-20 12:05:17 +0100

    Add travis

--- frontend/start-dev.sh
+++ frontend/start-dev.sh
@@ -3,6 +3,5 @@
 set -e
 set -x
 
-docker build -f Dockerfile.dev -t frontend_react .
 
-docker run -p "8001:3000"  -v /var/www/frontend/node_modules -v $(pwd):/var/www/frontend frontend_react
+docker-compose -f docker-compose-dev.yml up --build
